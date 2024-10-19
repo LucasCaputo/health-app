c35af2e457b5d4234715716da413c34b3b7138ee
Lucas Caputo <lccaputo@hotmail.com>
2024-10-19 09:58:36 -0300

    feat: add initial Angular 18 project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { NewAccountComponent } from './pages/new-account/new-account.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: 'new-account', component: NewAccountComponent },
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: '**', redirectTo: '/login' }
+];
