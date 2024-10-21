ff09c935925f89036c7a0f28719e69aa9e75b0a8
Lucas Caputo <lccaputo@hotmail.com>
2024-10-21 14:43:23 -0300

    chore: update API URL constant to use HTTPS

--- src/app/shared/contants/api-url.contant.ts
+++ src/app/shared/contants/api-url.contant.ts
@@ -1,2 +1,2 @@
 // constants/api-url.constant.ts
-export const API_URL = 'http://18.118.50.0:8080/api'; // Replace with your API URL
+export const API_URL = 'https://app-saude-onrender.com'; // Replace with your API URL
